5e166e3ec400e154a0d4779d671a6ce2aea439e4
rimo <munak.dev@gmail.com>
2024-01-28 23:06:17 +0900

    feat : add counter fuction

--- docker-compose-app/src/server.js
+++ docker-compose-app/src/server.js
@@ -8,5 +8,16 @@ const client = redis.createClient({
 });
 
 const app = express();
+
+// Counter App
+client.set("number", 0);
+
+app.get("/", (req, res) => {
+    client("number", (err, number) => {
+        client.set("number", parseInt(number) + 1);
+        res.send("counter : " + number);
+    });
+});
+
 app.listen(8080);
 console.log("server is running");
