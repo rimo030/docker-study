c8b21d9b3d273125a0238c037a880bf5c271b53e
rimo <munak.dev@gmail.com>
2024-01-29 22:13:44 +0900

    fix : modify redis connection

--- docker-compose-app/src/server.js
+++ docker-compose-app/src/server.js
@@ -3,21 +3,31 @@ const redis = require("redis");
 
 // Create redis client
 const client = redis.createClient({
-    host: "redis-server",
-    port: 6379,
+    socket: {
+        host: "redis-server",
+        port: 6379,
+    },
 });
+
+client.on("error", (err) => console.log("Redis Client Error", err));
 
 const app = express();
 
 // Counter App
-client.set("number", 0);
 
-app.get("/", (req, res) => {
-    client("number", (err, number) => {
-        client.set("number", parseInt(number) + 1);
-        res.send("counter : " + number);
-    });
+app.get("/", async (req, res) => {
+    await client.connect();
+
+    let number = await client.get("number");
+    if (number === null) {
+        number = 0;
+    }
+
+    res.send("counter : " + number);
+
+    await client.set("number", parseInt(number) + 1);
+    await client.disconnect();
 });
 
 app.listen(8080);
-console.log("server is running");
+console.log("Server is running");
